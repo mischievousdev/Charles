014612b08756df2ea47931c42667c84df1153e68
iDutchy <speedyniels@gmail.com>
2019-10-30 17:42:05 -0700

    check fix

--- cogs/Music.py
+++ cogs/Music.py
@@ -1162,7 +1162,7 @@ class Music(commands.Cog, name="Music"):
 
         await ctx.send(get_text(ctx.guild, 'music', 'music.pl.song_added').format(title, playlist))
 
-    @music_check(no_channel=True, bot_no_channel=True, same_channel=True)
+    @music_check(no_channel=True)
     @playlist.command(aliases=['play'])
     async def start(self, ctx, *, playlist):
         player = self.bot.wavelink.get_player(ctx.guild.id, cls=Player)
