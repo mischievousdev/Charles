554c0a1e6c5e41031108af608b9842443386358a
iDutchy <42503862+iDutchy@users.noreply.github.com>
2019-11-08 03:04:12 +0100

    Deleted unused code

--- utils/default.py
+++ utils/default.py
@@ -21,20 +21,6 @@ class GroupPlus(commands.Group):
 
 def groupExtra(*args, **kwargs):
     return commands.group(*args, **kwargs, cls=GroupPlus)
-    
-
-def get(file):
-    try:
-        with open(file, encoding='utf8') as data:
-            return json.load(data, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
-    except AttributeError:
-        raise AttributeError("Unknown argument")
-    except FileNotFoundError:
-        raise FileNotFoundError("JSON file wasn't found")
-
-
-def timetext(name):
-    return f"{name}_{int(time.time())}.txt"
 
 
 def timeago(target):
@@ -46,18 +32,3 @@ def date(target, clock=True):
         return target.strftime("%d %B %Y")
     return target.strftime("%d %B %Y, %H:%M")
 
-
-def responsible(target, reason):
-    responsible = f"[ {target} ]"
-    if reason is None:
-        return f"{responsible} no reason given..."
-    return f"{responsible} {reason}"
-
-
-def actionmessage(case, mass=False):
-    output = f"**{case}** the user"
-
-    if mass is True:
-        output = f"**{case}** the IDs/Users"
-
-    return f"✅ Successfully {output}"
